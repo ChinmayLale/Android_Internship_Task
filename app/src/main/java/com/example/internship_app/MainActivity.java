b63fa32065501661e5952e29e7544c5cba3a731c
ChinmayLale <chinmay29.lale@gmail.com>
2024-01-29 19:46:07 +0530

    First Commite Android Project On GitHub

---
+++ app/src/main/java/com/example/internship_app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.internship_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
